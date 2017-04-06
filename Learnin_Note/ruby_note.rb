1818b912c7926add47a27e88d88abe58e517fab8
BillKung <billkung.happy@gmail.com>
2017-04-06 16:32:37 +0800

    Put my new workspace on github

---
+++ Learnin_Note/ruby_note.rb
@@ -0,0 +1,18 @@
+ruby note 
+
+functions
+print "hello"
+# output String
+puts "hi"
+# output String then add turn to next line
+"ggffddd".length
+# get the length of the String
+"abcd".reverse
+# reverse the String
+.upcase
+.downcase
+# 大小寫
+
+=begin
+many lines comment!
+=end
---
+++ learning_test/ruby/test.rb
@@ -0,0 +1,7 @@
+print "hello"
+# output String
+puts "hi"
+# output String then add turn to next line
+strlen= "ggffddd".length
+# get the length of the String
+puts strlen
